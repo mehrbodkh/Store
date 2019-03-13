ec505bb52f1e5f8987de5647d4562c06ee9f5570
Nader Abolfazli <abolfazli.nader@gmail.com>
2019-03-13 12:55:19 +0330

    ref: add db config

---
+++ DB/config.py
@@ -0,0 +1,10 @@
+import os
+
+
+class DbConfig:
+    db_user = os.getenv('POSTGRES_USER', "")
+    db_password = os.getenv('POSTGRES_PASSWORD', "")
+    db_host = os.getenv('POSTGRES_HOST', "localhost")
+    db_name = os.getenv('POSTGRES_DB', "store_db")
+    db_port = os.getenv('POSTGRES_PORT', "5432")
+    database_url = "postgresql://{}:{}@{}:{}/{}".format(db_user, db_password, db_host, db_port, db_name) or None
