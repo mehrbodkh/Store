ab396a14c225f76986891549681fcea129be7f60
Nader Abolfazli <abolfazli.nader@gmail.com>
2019-03-13 13:49:38 +0330

    feat: add address model

---
+++ DB/models/address.py
@@ -0,0 +1,18 @@
+from sqlalchemy import Column, Integer, String
+
+from DB.models.base import Base
+
+
+class Address(Base):
+    __tablename__ = "address"
+    id = Column(Integer, primary_key=True)
+    city = Column(String)
+    address = Column(String)
+    latitude = Column(String)
+    longitude = Column(String)
+
+    def __init__(self, city, address, latitude, longitude):
+        self.city = city
+        self.address = address
+        self.latitude = latitude
+        self.longitude = longitude
