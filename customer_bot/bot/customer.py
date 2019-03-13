81e3f1aaf19f66b60cc20dece0d511861ea1c193
baleadmin <dr.enyacc@gmail.com>
2019-03-13 12:42:09 +0330

    feat(): Add user_data to states

--- customer_bot/bot/customer.py
+++ customer_bot/bot/customer.py
@@ -19,39 +19,46 @@ SORT_TYPE, STORE, CATEGORY, BIO = range(4)
 
 def start(bot, update):
     reply_keyboard = [[ReplyKeyboards.stores, ReplyKeyboards.products]]
-    logger.info("start called")
     update.message.reply_text(BotMessages.start, reply_markup=ReplyKeyboardMarkup(keyboard=reply_keyboard))
     return SORT_TYPE
 
 
-
-
+# ++++++++++++++++++++++++++ choose store scenario ++++++++++++++++++++++++++++
 def stores(bot, update):
-    logger.info("in stores")
     user = update.message.from_user
     store_list = get_store_list()
-    reply_keyboard = [store_list]
-    logger.info("Gender of %s: %s", user.first_name, update.message.text)
+    store_name_list = get_name_list_from_store(store_list)
+    reply_keyboard = [store_name_list]
     update.message.reply_text(BotMessages.stores, reply_markup=ReplyKeyboardMarkup(keyboard=reply_keyboard))
 
     return STORE
 
 
-def product_category_by_store(bot, update):
-    logger.info("in stores")
+def product_category_by_store(bot, update, user_date):
     user = update.message.from_user
-    logger.info("Gender of %s: %s", user.first_name, update.message.text)
-    update.message.reply_text('I see! Please send me a photo of yourself, '
-                              'so I know what you look like, or send /skip if you don\'t want to.')
+    store_list = get_store_list()
+    user_date[store_list] = store_list
+    store_name_list = get_name_list_from_store(store_list)
+    reply_keyboard = [store_name_list]
+    update.message.reply_text(BotMessages.choose_category, reply_markup=ReplyKeyboardMarkup(keyboard=reply_keyboard))
     return CATEGORY
 
 
+def product_category_by_store(bot, update, user_date):
+    user = update.message.from_user
+    store_list = get_store_list()
+
+    store_name_list = get_name_list_from_store(store_list)
+    reply_keyboard = [store_name_list]
+    update.message.reply_text(BotMessages.choose_category, reply_markup=ReplyKeyboardMarkup(keyboard=reply_keyboard))
+    return CATEGORY
+
+
+# ++++++++++++++++++++++++++ choose sort type ++++++++++++++++++++++++++++
 def products(bot, update):
     user = update.message.from_user
     photo_file = update.message.photo[-1].get_file()
-    logger.info(photo_file)
     photo_file.download('user_photo.jpg')
-    logger.info("Photo of %s: %s", user.first_name, 'user_photo.jpg')
     update.message.reply_text('Gorgeous! Now, send me your location please, '
                               'or send /skip if you don\'t want to.')
     return CATEGORY
@@ -59,7 +66,6 @@ def products(bot, update):
 
 def skip_photo(bot, update):
     user = update.message.from_user
-    logger.info("User %s did not send a photo.", user.first_name)
     update.message.reply_text('I bet you look great! Now, send me your location please, '
                               'or send /skip.')
 
@@ -69,8 +75,6 @@ def skip_photo(bot, update):
 def location(bot, update):
     user = update.message.from_user
     user_location = update.message.location
-    logger.info("Location of %s: %f / %f", user.first_name, user_location.latitude,
-                user_location.longitude)
     update.message.reply_text('Maybe I can visit you sometime! '
                               'At last, tell me something about yourself.')
 
@@ -79,7 +83,6 @@ def location(bot, update):
 
 def skip_location(bot, update):
     user = update.message.from_user
-    logger.info("User %s did not send a location.", user.first_name)
     update.message.reply_text('You seem a bit paranoid! '
                               'At last, tell me something about yourself.')
 
@@ -88,7 +91,6 @@ def skip_location(bot, update):
 
 def bio(bot, update):
     user = update.message.from_user
-    logger.info("Bio of %s: %s", user.first_name, update.message.text)
     update.message.reply_text('Thank you! I hope we can talk again some day.')
 
     return ConversationHandler.END
@@ -96,7 +98,6 @@ def bio(bot, update):
 
 def cancel(bot, update):
     user = update.message.from_user
-    logger.info("User %s canceled the conversation.", user.first_name)
     update.message.reply_text('Bye! I hope we can talk again some day.')
 
     return ConversationHandler.END
