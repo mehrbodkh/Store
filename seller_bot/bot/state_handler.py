f3a4582a9d2f38c86b48b7f8e749a0cc175c8aa6
Mehrbod Khiabani <mehrbod.kh@gmail.com>
2019-03-14 11:20:21 +0330

    feat(seller): Shows all orders with location.

--- seller_bot/bot/state_handler.py
+++ seller_bot/bot/state_handler.py
@@ -15,6 +15,7 @@ def main():
     dp.add_handler(show_all_items_message_handler)
     dp.add_handler(change_store_card_conversation_handler)
     dp.add_handler(help_command_handler)
+    dp.add_handler(get_orders_message_handler)
 
     dp.add_error_handler(error)
 
